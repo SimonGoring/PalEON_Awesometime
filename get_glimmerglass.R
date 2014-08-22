3cc3ef820559aaf01429432ef3bc91dae180b215
Simon <simon.j.goring@gmail.com>
2014-08-22 00:42:22 -0500

    Wrote the script to download Glimmerglass.

--- get_glimmerglass.R
+++ get_glimmerglass.R
@@ -1,5 +1,10 @@
-install.packages("devtools") 
+#install.packages("devtools") 
 require(devtools) 
 install_github("neotoma", "ropensci") 
 require(neotoma) 
+
+get.glimmer <- get_site(sitename='Glimm%')
+glimmer.dataset <- get_dataset(siteid = get.glimmer$siteid, datasettype='pollen')
+
+glimmerglass <- get_download(glimmer.dataset)
 
